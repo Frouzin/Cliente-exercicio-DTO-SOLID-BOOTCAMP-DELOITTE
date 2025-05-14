dba5003984912afcd8bf9b53083b2676ee830524
Frouzin <matheusereis44@gmail.com>
2025-05-14 14:16:15 -0300

    Bootcamp deloitte ClienteDTO PedidoDTO

---
+++ src/main/java/dto/PedidoDTO.java
@@ -0,0 +1,35 @@
+package dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class PedidoDTO {
+
+    private LocalDate data;
+    private List<ItemPedidoDTO> itens;
+
+    public PedidoDTO(){
+        this.data = LocalDate.now();
+    }
+
+    public PedidoDTO(LocalDate data, List<ItemPedidoDTO> itens){
+        this.data = data;
+        this.itens = itens;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public List<ItemPedidoDTO> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedidoDTO> itens) {
+        this.itens = itens;
+    }
+}
